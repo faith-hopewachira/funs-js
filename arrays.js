4088e70c8205c5609b9489e8137e4eb6e35442b4
FaithHope <wachiraafaith>
2024-04-04 11:45:37 +0300

    controlFlow

--- arrays.js
+++ arrays.js
@@ -21,6 +21,10 @@ let word3 = word2.split('').sort();
 console.log('sorted_array',word3.join(' '));
 
 
+let wordy = word2.split(" ").reverse().join(" ");
+console.log({'splittedWord': word2})
+
+
 //Write a JS script to sort the following array itemsvar arr3 = [ -5, 9, 5, 3, 2, -3, 6, 8, 4, 1 ];
 var arr3 = [-5, 9, 5, 3, 2, -3, 6, 8, 4, 1 ];
 //A - B = ANSWER = (-), THE 2ND NO IS BIGGER
@@ -36,8 +40,8 @@ const array = [1, 2, 3, 4, 2, 3, 5];
 // const uniqueArray = [...new Set(array)];
 // console.log(uniqueArray);
 // // Output: [1, 2, 3, 4, 5]
-let arr4 = [] //STORES DUPLICATES
-let arr4Original =[] //STORES ORIGINAL
+let arr4 = []; //STORES DUPLICATES
+let arr4Original = []; //STORES ORIGINAL
 array.forEach(fruit =>{
     if(!arr4Original.includes(fruit)){ //INCLUDE FRUIT AND DO NOT REPEAT ITSELF
         arr4Original.push(fruit)
@@ -45,10 +49,9 @@ array.forEach(fruit =>{
     else{
         arr4.push(fruit) // WHATEVER IS NOT UNIQUE
     }
-})
-console.log({arr4})
-console.log({arr4Original})
-
+});
+console.log({arr4});
+console.log({arr4Original});
 
 
 //Write a JS script to search for the following word in the array."way"If the word is present, 
